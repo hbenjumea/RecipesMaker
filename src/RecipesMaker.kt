834b431c4197d7e7089f07737f222ff9b5ede03d
Hugo Andres Benjumea Alvear <hugo_andresb@hotmail.com>
2019-12-13 10:24:46 -0500

    Modularizar el menu

--- src/RecipesMaker.kt
+++ src/RecipesMaker.kt
@@ -16,17 +16,21 @@ fun main() {
 
         val response:String? = readLine()
 
-        //val numero = response?.toInt()
-
         when(response){
-            "1" -> {
-                receta = ""
-                do {
-                    val menu2 = """
-                    :: Ingredientes ::
-                    
-                    
-                    Selecciona la opción deseada
+            "1" -> receta = makeRecipe()
+            "2" -> viewRecipe(receta)
+            "3" -> break@menuprincipal
+            else -> println("Solo 1, 2, 3")
+        }
+    }while(response != "3")
+}
+
+fun makeRecipe():String{
+    var recipe = ""
+    do {
+        val menu2 = """
+                    Hacer receta
+                    Selecciona por categoría el ingrediente que buscas
                     1. Agua
                     2. Leche
                     3. Carne
@@ -34,31 +38,26 @@ fun main() {
                     5. Frutas
                     6. Cereal
                     7. Huevos
-                    8. Aceite
+                    8. Aceites
                     9. Regresar
                 """.trimIndent()
-                    println(menu2)
-                    val response2:String? = readLine()
-
-                    when(response2){
-                        "1" -> receta += "Agua, "
-                        "2" -> receta += "Leche, "
-                        "3" -> receta += "Carne, "
-                        "4" -> receta += "Verduras, "
-                        "5" -> receta += "Frutas, "
-                        "6" -> receta += "Cereal, "
-                        "7" -> receta += "Huevos, "
-                        "8" -> receta += "Aceite, "
-                        "9" -> continue@menuprincipal
-                        else -> println("Solo del 1 al 9")
-                    }
-                }while(response2 != "9")
-            }
-            "2" -> println("receta: $receta")
-            "3" -> break@menuprincipal
-            else -> println("Solo 1, 2, 3")
+        println(menu2)
+        val response2:String? = readLine()
+        when(response2){
+            "1" -> recipe += "Agua, "
+            "2" -> recipe += "Leche, "
+            "3" -> recipe += "Carne, "
+            "4" -> recipe += "Verduras, "
+            "5" -> recipe += "Frutas, "
+            "6" -> recipe += "Cereal, "
+            "7" -> recipe += "Huevos, "
+            "8" -> recipe += "Aceites, "
+            else -> println("Solo del 1 al 9")
         }
-    }while(response != "3")
+    }while(response2 != "9")
+    return recipe
+}
 
-
+fun viewRecipe(receta: String){
+    println("receta: $receta")
 }
